c4e9e21ab005157fdf0f48feed9116342fef6d41
parkyejin <uiop5809@naver.com>
2022-04-03 13:27:32 +0900

    22.04.03

---
+++ Algorithm/11719.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <cstring>
+#include <stack>
+#include <queue>
+#include <string>
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable : 4996)
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	string str;
+	while (getline(cin, str)) {
+		cout << str << "\n";
+	}
+}
