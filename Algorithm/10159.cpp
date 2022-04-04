a0908104e6c261adeddc79226386908f9065566f
parkyejin <uiop5809@naver.com>
2022-04-04 12:58:52 +0900

    22.04.04

---
+++ Algorithm/10159.cpp
@@ -0,0 +1,54 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <cstring>
+#include <stack>
+#include <queue>
+#include <cmath>
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable : 4996)
+using namespace std;
+
+int N, M, x, y;
+int num[101][101];
+const int INF = 1e9;
+
+void floyd() {
+	for (int k = 1; k <= N; k++) {
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N; j++) {
+				if (num[i][j] > num[i][k] + num[k][j]) {
+					num[i][j] = num[i][k] + num[k][j];
+				}
+			}
+		}
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> N >> M;
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
+			if (i != j) num[i][j] = INF;
+		}
+	}
+	for (int i = 0; i < M; i++) {
+		cin >> x >> y;
+		num[y][x] = 1; // y > x
+	}
+	floyd();
+
+	for (int i = 1; i <= N; i++) {
+		int cnt = 0;
+		for (int j = 1; j <= N; j++) {
+			if (i == j) continue;
+			if (num[i][j] == INF && num[j][i] == INF) {
+				cnt++;
+			}
+		}
+		cout << cnt << "\n";
+	}
+}
