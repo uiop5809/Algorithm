7ab6a1d5d62ffed9a28a3ccc767dec29f0364a00
parkyejin <uiop5809@naver.com>
2022-05-08 14:01:15 +0900

    22.05.08

--- Algorithm/1302.cpp
+++ Algorithm/1302.cpp
@@ -1,42 +1,33 @@
 #include <vector>
 #include <algorithm>
 #include <iostream>
-#include <stdio.h>
-#include <string.h>
+#include <cstdio>
 #include <string>
-#include <stack>
-#include <queue>
-#include <math.h>
-#define _CRT_SECURE_NO_WARNINGS
-#pragma warning(disable : 4996)
+#include <map>
 using namespace std;
+
+int N;
+string book;
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
 
-	int N;
-	long long n[100001];
-
 	cin >> N;
-	for (int i = 0; i < N; i++) {
-		cin >> n[i];
+	map <string, int> m;
+
+	while(N--){
+		cin >> book;
+		m[book]++;
 	}
-	sort(n, n + N);
-	
-	int cnt = 1, max = 0;
-	long long ans = n[0];
-	for (int i = 0; i < N - 1; i++) {
-		if (n[i] == n[i + 1]) {
-			cnt++;
-		}
-		else {
-			cnt = 1;
-		}
-		if (max < cnt) {
-			max = cnt;
-			ans = n[i];
+	int res = 0;
+	for (auto i:m) {
+		res = max(res, i.second);
+	}
+	for (auto i:m) {
+		if (res == i.second) {
+			cout << i.first;
+			return 0;
 		}
 	}
-	cout << ans;
 }
