144923e867455912141b1b06bb5abc3ca0ffa3b2
parkyejin <uiop5809@naver.com>
2022-05-09 17:57:04 +0900

    22.05.09

--- Algorithm/1003.cpp
+++ Algorithm/1003.cpp
@@ -1,14 +1,8 @@
 #include <vector>
 #include <algorithm>
 #include <iostream>
-#include <stdio.h>
-#include <string.h>
-#include <string>
 #include <stack>
 #include <queue>
-#include <math.h>
-#define _CRT_SECURE_NO_WARNINGS
-#pragma warning(disable : 4996)
 using namespace std;
 
 int T, num;
