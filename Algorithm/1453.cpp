8f7e85d1c706aaebeecfb0d63782d32aaf4b556c
parkyejin <uiop5809@naver.com>
2022-05-18 14:54:49 +0900

    22.05.18

---
+++ Algorithm/1453.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <stack>
+#include <queue>
+#include <cmath>
+using namespace std;
+
+int N, num, s[101], cnt;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> num;
+		s[num]++;
+		if (s[num] != 1) cnt++;
+	}
+	cout << cnt;
+}
