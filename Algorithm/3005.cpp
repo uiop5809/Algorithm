13204c477070029abbf5d951b803ea555a9686b4
parkyejin <uiop5809@naver.com>
2022-05-31 00:13:45 +0900

    22.05.31

---
+++ Algorithm/3005.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+using namespace std;
+
+int R, C;
+string str[21];
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> R >> C;
+	for (int i = 0; i < R; i++) {
+		cin >> str[i];
+	}
+	vector<string> v;
+	for (int i = 0; i < R; i++) {
+		string res = "";
+		for (int j = 0; j < C; j++) {
+			if (str[i][j] == '#') {
+				v.push_back(res);
+				res = "";
+			}
+			else res += str[i][j];
+		}
+		if (res != "") v.push_back(res);
+	}
+	for (int i = 0; i < C; i++) {
+		string res = "";
+		for (int j = 0; j < R; j++) {
+			if (str[j][i] == '#') {
+				v.push_back(res);
+				res = "";
+			}
+			else res += str[j][i];
+		}
+		if (res != "") v.push_back(res);
+	}
+
+	sort(v.begin(), v.end());
+	for (int i = 0; i < v.size(); i++) {
+		if (v[i].size() >= 2) {
+			cout << v[i];
+			break;
+		}
+	}
+}
