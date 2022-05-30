ceb911c225aab7c9eb5b7533833a25a2c296f03f
parkyejin <uiop5809@naver.com>
2022-05-31 00:13:39 +0900

    22.05.31

--- Algorithm/2630.cpp
+++ Algorithm/2630.cpp
@@ -1,49 +1,37 @@
-#include <vector>
-#include <algorithm>
 #include <iostream>
-#include <stdio.h>
-#include <string.h>
-#include <string>
-#include <stack>
-#include <queue>
-#include <math.h>
-#define _CRT_SECURE_NO_WARNINGS
-#pragma warning(disable : 4996)
 using namespace std;
 
 int white, blue;
-int cp[130][130];
+int arr[130][130];
 
-void get_cnt(int x, int y, int size) {
-	int color = cp[x][y];
-	for (int i = x; i < x + size; i++) {
-		for (int j = y; j < y + size; j++) {
-			if (cp[i][j] != color) {
-				get_cnt(x, y, size / 2); //제 2사분면
-				get_cnt(x, y + (size / 2), size / 2); //제 1사분면
-				get_cnt(x + (size / 2), y, size / 2); //제 3사분면
-				get_cnt(x + (size / 2), y + (size / 2), size / 2); //제 4사분면
+void get_cnt(int y, int x, int size) {
+	int color = arr[y][x];
+	for (int i = y; i < y + size; i++) {
+		for (int j = x; j < x + size; j++) {
+			if (color != arr[i][j]) {
+				get_cnt(y, x, size / 2); // 왼쪽 위
+				get_cnt(y, x + size / 2, size / 2); // 오른쪽 위
+				get_cnt(y + size / 2, x, size / 2); // 왼쪽 아래
+				get_cnt(y + size / 2, x + size / 2, size / 2); // 오른쪽 아래
 				return;
 			}
 		}
 	}
-	if (color == 1) {
-		blue++;
-	}
-	else {
-		white++;
-	}
+	if (color == 1) blue++;
+	else if (color == 0) white++;
 }
 
 int main() {
-	int n, m;
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
 
+	int n, m;
 	cin >> n;
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
-			cin >> cp[i][j];
+			cin >> arr[i][j];
 		}
 	}
-	get_cnt(0, 0, n);
+	get_cnt(0, 0, n); // y, x, size
 	cout << white << "\n" << blue;
 }
