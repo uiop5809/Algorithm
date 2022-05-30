53c42ed9024e8f6eea6eb007d7593ad6069afb00
parkyejin <uiop5809@naver.com>
2022-05-31 00:11:57 +0900

    22.05.31

--- Algorithm/10216.cpp
+++ Algorithm/10216.cpp
@@ -1,54 +1,45 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <cmath>
 using namespace std;
 
-int m, n, x, y, z, parent[200001];
+int T, N, x, y, R, parent[10001];
+
+vector <pair <int, pair <int, int>>> v;
 
 int getParent(int x) {
-	if (parent[x] == x) return x;
-	return parent[x] = getParent(parent[x]);
+	if (x == parent[x]) return x;
+	else parent[x] = getParent(parent[x]);
+}
+
+void unionParent(int a, int b) {
+	a = getParent(a);
+	b = getParent(b);
+	if (a > b) parent[a] = b;
+	else parent[b] = a;
 }
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
-	while (1) {
-		cin >> m >> n;
-		if (m == 0 && n == 0) break;
-		for (int i = 0; i < m; i++) {
+	cin >> T;
+	while (T--) {
+		cin >> N;
+		for (int i = 1; i <= N; i++) {
 			parent[i] = i;
 		}
-
-		int sum = 0;
-		vector <pair <int, pair <int, int>>> v;
-		for (int i = 0; i < n; i++) {
-			cin >> x >> y >> z;
-			v.push_back({ z, {x, y} });
-			sum += z;
+		for (int i = 0; i < N; i++) {
+			cin >> x >> y >> R;
+			v.push_back({ R, {x, y} });
 		}
-		sort(v.begin(), v.end());
-		
-		int cnt = 0;
-		int res = 0;
-		for (int i = 0; i < n; i++) {
-			int cost = v[i].first;
-			int start = v[i].second.first;
-			int end = v[i].second.second;
+		for (int i = 0; i < N; i++) {
+			for (int j = i + 1; j < N; j++) {
+				if (i == j) continue;
+				if (pow(v[i].first )) {
 
-			int x = getParent(start);
-			int y = getParent(end);
-
-			if (x == y) continue;
-			if (x > y) parent[x] = y;
-			else parent[y] = x;
-
-			res += cost;
-			cnt++;
-			if (cnt == m - 1) {
-				cout << sum - res << "\n";
-				break;
+				}
 			}
 		}
 	}
