7a82c05ff7b370b1414b0f6acb19a41848fd4d66
parkyejin <uiop5809@naver.com>
2022-06-28 20:26:27 +0900

    22.06.28

--- Algorithm/1254.cpp
+++ Algorithm/1254.cpp
@@ -1,50 +1,15 @@
 #include <vector>
 #include <algorithm>
 #include <iostream>
-#include <stdio.h>
-#include <string.h>
-#include <string>
-#include <stack>
-#include <queue>
-#include <math.h>
-#define _CRT_SECURE_NO_WARNINGS
-#pragma warning(disable : 4996)
+#include <cstring>
 using namespace std;
 
-int T, V, E, x, y;
+string str;
 
-vector <int> v[200001];
-int color[200001]; // 1 »¡°­, 2 ÆÄ¶û
-
-queue <int> q;
-
-void bfs(int x) {
-	q.push(x);
-	color[x] = 1;
-
-	while (!q.empty()) {
-		int x = q.front();
-		q.pop();
-
-		for (int i = 0; i < v[x].size(); i++) {
-			int nx = v[x][i];
-
-			if (!color[nx]) {
-				q.push(nx);
-
-				if (color[x] == 1) color[nx] = 2;
-				else if (color[x] == 2) color[nx] = 1;
-			}
-		}
-	}
-}
-
-bool result() {
-	for (int i = 1; i <= V; i++) {
-		for (int j = 0; j < v[i].size(); j++) {
-			int nx = v[i][j];
-			if (color[i] == color[nx]) return false;
-		}
+bool palindrome(int idx) {
+	int half = (str.size() - idx) / 2;
+	for (int i = 0; i < half; i++) {
+		if (str[i + idx] != str[str.size() - 1 - i]) return false;
 	}
 	return true;
 }
@@ -53,23 +18,12 @@ int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
-	cin >> T;
-	while (T--) {
-		cin >> V >> E;
-		for (int i = 0; i < E; i++) {
-			cin >> x >> y;
-			v[x].push_back(y);
-			v[y].push_back(x);
+	cin >> str;
+	int size = str.size();
+	for (int i = 0; i < size; i++) {
+		if (palindrome(i)) {
+			cout << size + i;
+			break;
 		}
-		for (int i = 1; i <= V; i++) {
-			if (!color[i]) {
-				bfs(i);
-			}
-		}
-		if (result()) cout << "YES" << "\n";
-		else cout << "NO" << "\n";
-
-		memset(color, 0, sizeof(color));
-		memset(v, 0, sizeof(v));
 	}
 }
