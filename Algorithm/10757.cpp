8ad73632c42c546072a88d1f6fbe7d44c3096831
parkyejin <uiop5809@naver.com>
2022-07-28 20:20:00 +0900

    22.07.28

--- Algorithm/10757.cpp
+++ Algorithm/10757.cpp
@@ -1,41 +1,43 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
+#include <string>
 using namespace std;
+
+int num1[10001], num2[10001], sum;
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
-	string A, B;
-	cin >> A >> B;
-	if (A.size() < B.size()) swap(A, B);
-	reverse(A.begin(), A.end());
-	reverse(B.begin(), B.end());
+	string s1, s2;
+	cin >> s1 >> s2;
+	if (s1.size() < s2.size()) swap(s1, s2);
 
-	if (A.size() == B.size()) {
-		for (int i = 0; i < B.size(); i++) {
-			int sum = (A[i] - '0') + (B[i] - '0');
-			if (sum >= 10) {
-				sum -= 10;
-				A[i] = sum + '0';
-				A[i + 1] += 1;
-				if (i == B.size() - 1) A += '1';
-			}
-			else A[i] += B[i] - '0';
-		}
+	for (int i = 0; i < s1.size(); i++) {
+		num1[i] = s1[s1.size() - 1 - i] - '0';
 	}
+	for (int i = 0; i < s2.size(); i++) {
+		num2[i] = s2[s2.size() - 1 - i] - '0';
+	}
+
+	vector<int> res;
+	for (int i = 0; i < s1.size(); i++) {
+		sum = num1[i] + num2[i];
+		if (sum >= 10) {
+			num1[i + 1]++;
+			sum -= 10;
+		}
+		res.push_back(sum);
+	}
+
+	bool flag = true;
+	if (num1[s1.size()] != 0) cout << 1;
 	else {
-		for (int i = 0; i < B.size(); i++) {
-			int sum = (A[i] - '0') + (B[i] - '0');
-			if (sum >= 10) {
-				sum -= 10;
-				A[i] = sum + '0';
-				A[i + 1] += 1;
-			}
-			else A[i] += B[i] - '0';
-		}
+		if (res[res.size() - 1] == 0) flag = false;
 	}
-
-	reverse(A.begin(), A.end());
-	cout << A;
+	for (int i = res.size() - 1; i >= 0; i--) {
+		if (res[i] != 0) flag = true;
+		if(flag) cout << res[i];
+	}
 }
