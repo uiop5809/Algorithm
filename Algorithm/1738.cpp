0fec2d3d82fe514f67522cbbdd98d0b6175fb2f9
parkyejin <uiop5809@naver.com>
2022-08-02 11:02:56 +0900

    22.08.02

--- Algorithm/1738.cpp
+++ Algorithm/1738.cpp
@@ -8,17 +8,19 @@
 #define _CRT_SECURE_NO_WARNINGS
 #pragma warning(disable : 4996)
 using namespace std;
-#define INF 2100000000
 
-int n, m, x, y, z;
-long long dist[101];
-long long res[101];
+int n, m, u, v, w;
+const int INF = 1e9;
 bool cycle;
+bool c[101];
+int pre[101];
 
+vector <int> rev[101]; // 정점 번호에서 역추적하여 닿을 수 있는 정점들 check
+vector <int> dist;
 vector <pair <int, int>> edge[101];
 
 void bellman_ford() {
-	fill(dist, dist + n + 1, INF);
+	dist.resize(n + 1, INF);
 	dist[1] = 0;
 
 	for (int k = 1; k <= n; k++) {
@@ -29,8 +31,8 @@ void bellman_ford() {
 
 				if (dist[i] != INF && dist[next] > dist[i] + nextcost) {
 					dist[next] = dist[i] + nextcost;
-					res[next] = i;
-					if (k == n) cycle = true;
+					pre[next] = i;
+					if (k == n && c[next]) cycle = true;
 				}
 			}
 		}
@@ -41,25 +43,43 @@ int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
+	// 금품의 많을수록 좋으니 양의 무한대가 되면 안 됨
+	// But 가중치 * -1 해줬으니 음의 무한대가 되면 안 됨
 	cin >> n >> m;
 	for (int i = 0; i < m; i++) {
-		cin >> x >> y >> z;
-		edge[x].push_back({ y, -z }); // 이웃노드, 가중치
+		cin >> u >> v >> w;
+		edge[u].push_back({ v, -w }); // 이웃노드, 가중치
+		rev[v].push_back(u);
 	}
-	bellman_ford();
 
-	vector <int> ans;
-	long long temp = n;
-	while (temp) {
-		ans.push_back(temp);
-		temp = res[temp];
+	// 사이클이 있다고 무조건 -1 출력 X
+	// 도착지점까지 이동 중, 음의 사이클이 존재하면 -1
+	queue <int> q;
+	q.push(n);
+	c[n] = 1;
+	while (!q.empty()) {
+		int now = q.front();
+		q.pop();
+		for (int i = 0; i < rev[now].size(); i++) {
+			int next = rev[now][i];
+			if (!c[next]) {
+				c[next] = 1;
+				q.push(next);
+			}
+		}
 	}
+
+	bellman_ford();
 
 	if (cycle) cout << -1;
 	else {
-		for (int i = ans.size() - 1; i >= 0; i--) {
-			cout << ans[i] << " ";
+		vector <int> res;
+		int temp = n;
+		while (temp) {
+			res.push_back(temp);
+			temp = pre[temp];
 		}
+		for (int i = res.size() - 1; i >= 0; i--) 
+			cout << res[i] << " ";
 	}
-	return 0;
 }
